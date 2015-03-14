9d01bf7cdaa0551a8a5b6e83a2cd1ca015eff701
Harold <Harold@Harold-PC>
2015-03-15 10:21:01 +1100

    Removed unecessary imports

--- mavenproject1/test/com/java/demo/BankAccountTest.java
+++ mavenproject1/test/com/java/demo/BankAccountTest.java
@@ -5,9 +5,7 @@
 package com.java.demo;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
